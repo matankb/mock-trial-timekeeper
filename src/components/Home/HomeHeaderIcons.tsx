0da0d367225ff6df506704cf0059a24a9386e66f
Matan Kotler-Berkowitz <205matan@gmail.com>
2025-10-14 14:24:35 -0400

    Use react-navigation/elements for header buttons
    They work better for Liquid Glass

--- src/components/Home/HomeHeaderIcons.tsx
+++ src/components/Home/HomeHeaderIcons.tsx
@@ -1,28 +1,28 @@
 import { Ionicons } from '@expo/vector-icons';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { FC } from 'react';
-import { TouchableOpacity } from 'react-native';
 
 import { RouteProps } from '../../Navigation';
 import colors from '../../constants/colors';
 import { ScreenName } from '../../constants/screen-names';
+import { HeaderButton } from '@react-navigation/elements';
 
 type HomeHeaderIconProps = NativeStackScreenProps<RouteProps, ScreenName.HOME>;
 
 export const HomeHeaderIconLeft: FC<HomeHeaderIconProps> = ({ navigation }) => (
-  <TouchableOpacity onPressOut={() => navigation.navigate(ScreenName.ABOUT)}>
+  <HeaderButton onPress={() => navigation.navigate(ScreenName.ABOUT)}>
     <Ionicons
       name="information-circle-outline"
       size={26}
       color={colors.HEADER_BLUE}
     />
-  </TouchableOpacity>
+  </HeaderButton>
 );
 
 export const HomeHeaderIconRight: FC<HomeHeaderIconProps> = ({
   navigation,
 }) => (
-  <TouchableOpacity onPressOut={() => navigation.navigate(ScreenName.SETTINGS)}>
+  <HeaderButton onPress={() => navigation.navigate(ScreenName.SETTINGS)}>
     <Ionicons name="settings-outline" size={25} color={colors.HEADER_BLUE} />
-  </TouchableOpacity>
+  </HeaderButton>
 );
