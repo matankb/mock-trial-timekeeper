e640afadcee9a71b16ef64edd07a3ec7d355b7e3
Matan Kotler-Berkowitz <205matan@gmail.com>
2025-12-01 15:11:15 -0500

    Add inline and subtitle to link

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -8,30 +8,36 @@ import useTheme from '../hooks/useTheme';
 
 interface LinkProps {
   title: string;
+  subtitle?: string;
   onPress: () => void;
   border?: boolean;
+  inline?: boolean;
 }
 
-const Link: FC<LinkProps> = ({ title, onPress, border }) => {
+const Link: FC<LinkProps> = ({ title, subtitle, onPress, border, inline }) => {
   const theme = useTheme();
 
   return (
     <Card
-      style={{
-        ...styles.container,
-        ...(border ? styles.containerBorder : {}),
-      }}
+      style={StyleSheet.flatten([
+        styles.container,
+        border && styles.containerBorder,
+        inline && styles.containerInline,
+      ])}
     >
       <TouchableOpacity onPress={onPress}>
         <View style={styles.internalContainer}>
-          <Text
-            style={{
-              ...styles.text,
-              ...(theme === Theme.DARK && { color: 'white' }),
-            }}
-          >
-            {title}
-          </Text>
+          <View style={styles.titleContainer}>
+            <Text
+              style={{
+                ...styles.text,
+                ...(theme === Theme.DARK && { color: 'white' }),
+              }}
+            >
+              {title}
+            </Text>
+            {subtitle && <Text style={styles.subtitle}>{subtitle}</Text>}
+          </View>
           <MaterialIcons name="navigate-next" size={25} color="gray" />
         </View>
       </TouchableOpacity>
@@ -49,15 +55,27 @@ const styles = StyleSheet.create({
     borderColor: 'lightgray',
     borderWidth: 1,
   },
+  containerInline: {
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
   internalContainer: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    gap: 10,
+  },
   text: {
     fontSize: 16,
-    // color: 'white',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: 'gray',
   },
 });
 export default Link;
