1b0609d3d87f31d5963edf6ccbf65124c6f82a39
Matan Kotler-Berkowitz <205matan@gmail.com>
2025-12-04 11:40:55 -0500

    Add league and league feature flag constants

---
+++ src/constants/leagues.ts
@@ -0,0 +1,28 @@
+export enum League {
+  AMTA = 'amta',
+  Minnesota = 'minnesota',
+}
+
+export const leagueNames: Record<League, string> = {
+  [League.AMTA]: 'College Mock Trial',
+  [League.Minnesota]: 'Minnesota High School Mock Trial',
+};
+
+export enum LeagueFeature {
+  TIMES_BREAKDOWN = 'times-breakdown',
+  TEAM_ACCOUNTS = 'team-accounts',
+  WITNESS_SELECTION = 'witness-selection',
+}
+
+export const leagueFeatures: Record<League, Record<LeagueFeature, boolean>> = {
+  [League.AMTA]: {
+    [LeagueFeature.TIMES_BREAKDOWN]: true,
+    [LeagueFeature.TEAM_ACCOUNTS]: true,
+    [LeagueFeature.WITNESS_SELECTION]: true,
+  },
+  [League.Minnesota]: {
+    [LeagueFeature.TIMES_BREAKDOWN]: false,
+    [LeagueFeature.TEAM_ACCOUNTS]: false,
+    [LeagueFeature.WITNESS_SELECTION]: false,
+  },
+};
