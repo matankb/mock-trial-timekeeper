e2b0f91e3c2f0bc8d43aec9451bc019f10c48696
Matan Kotler-Berkowitz <205matan@gmail.com>
2025-12-04 11:45:13 -0500

    Add warning red constant

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -4,6 +4,7 @@ export default {
   GREEN: '#159e71',
   BRIGHT_GREEN: '#1bd195',
   RED: '#ff4444',
+  WARNING_RED: '#ac3636',
   BACKGROUND_GRAY: '#1c1c1e',
   PLACEHOLDER_GRAY: '#8e8e93',
   ORANGE: '#ff9500',
