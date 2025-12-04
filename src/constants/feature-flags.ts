7628116874d3931930d139b12b98dda3fcd93765
Matan Kotler-Berkowitz <205matan@gmail.com>
2025-12-04 11:40:07 -0500

    Disable global team trials in app feature flag

--- src/constants/feature-flags.ts
+++ src/constants/feature-flags.ts
@@ -1,3 +1,4 @@
 // Disabled October 2025, since some more work is neede to bring
 // team accounts to GA.
 export const TEAM_ACCOUNT_PROMO_ENABLED = false;
+export const TEAM_TRIALS_IN_APP_ENABLED = false;
