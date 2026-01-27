7f957c57314c9084d460f8682200cae89211af1a
Matan Kotler-Berkowitz <205matan@gmail.com>
2026-01-27 14:02:29 -0500

    Remove witness selection promo

--- src/components/CreateTrial/TrialDetails/WitnessSelector/WitnessSelectorInline.tsx
+++ src/components/CreateTrial/TrialDetails/WitnessSelector/WitnessSelectorInline.tsx
@@ -1,76 +1,23 @@
-import { Promos } from '../../../../controllers/promos';
-import { usePromo } from '../../../../hooks/usePromos';
-import CreateTrialSection from '../../CreateTrialSection';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
-import Promo from '../../../Home/Promos/Promo';
-import { MaterialIcons } from '@expo/vector-icons';
-import WitnessSelector from './WitnessSelector';
-import colors from '../../../../constants/colors';
-import Text from '../../../Text';
+import CreateTrialSection from "../../CreateTrialSection";
+import WitnessSelector from "./WitnessSelector";
+import colors from "../../../../constants/colors";
+import { League } from "../../../../constants/leagues";
+import { FC } from "react";
 
-export const WitnessSelectorInline = () => {
-  const promo = usePromo(Promos.WITNESS_CALL);
+interface WitnessSelectorInlineProps {
+  league: League;
+}
 
-  if (promo.loading) {
-    return null;
-  }
-
+export const WitnessSelectorInline: FC<WitnessSelectorInlineProps> = ({
+  league,
+}) => {
   return (
     <CreateTrialSection
       title="Witness Call"
       color={colors.ORANGE}
       subtitle="Optional"
     >
-      {promo.enabled && (
-        <View>
-          <View style={styles.promoContainer}>
-            <Promo
-              inline
-              title="Introducing Witness Calls"
-              subtitle="You can now optionally add the witness call order to your trial, letting you see the witness name for each examination, instead of just a number."
-              badge="NEW"
-              badgeColor={colors.ORANGE}
-            />
-          </View>
-          <TouchableOpacity
-            style={styles.buttonContainer}
-            onPress={() => promo.dismiss()}
-          >
-            <Text style={styles.buttonText}>Set witness call</Text>
-            <MaterialIcons
-              name="navigate-next"
-              size={25}
-              color={colors.ORANGE}
-              style={styles.buttonArrow}
-            />
-          </TouchableOpacity>
-        </View>
-      )}
-      {!promo.enabled && <WitnessSelector inline />}
+      <WitnessSelector inline league={league} />
     </CreateTrialSection>
   );
 };
-
-const styles = StyleSheet.create({
-  buttonContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    paddingHorizontal: 10,
-    paddingBottom: 10,
-  },
-  buttonArrow: {
-    // offset slight visual difference between text and arrow, despite align-items: center
-    marginTop: 2,
-  },
-  promoContainer: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-  },
-  buttonText: {
-    fontSize: 18,
-    paddingLeft: 10,
-    color: colors.ORANGE,
-  },
-});
