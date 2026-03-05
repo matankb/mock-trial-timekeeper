17de8e3a689223e64c04a2e5d6b5640bc1e9ec83
Matan Kotler-Berkowitz <205matan@gmail.com>
2026-03-05 09:23:22 -0500

    Add error highlighting to TextInput component

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,30 +1,37 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
 import {
   StyleSheet,
   TextInput as NativeTextInput,
   TextInputProps as NativeTextInputProps,
-} from "react-native";
+} from 'react-native';
+import colors from '../constants/colors';
 
 interface TextInputProps extends NativeTextInputProps {
   border?: boolean;
   inline?: boolean;
   full?: boolean;
+  error?: boolean;
 }
 
 const TextInput: FC<TextInputProps> = ({
   border = false,
   inline = false,
   full = false,
+  placeholderTextColor = colors.PLACEHOLDER_GRAY,
+  error = false,
   ...props
 }) => {
   return (
     <NativeTextInput
       {...props}
+      placeholderTextColor={placeholderTextColor}
       style={[
         styles.input,
         border && styles.border,
         inline && styles.inline,
         full && styles.full,
+        error && styles.error,
+        props.style,
       ]}
     />
   );
@@ -34,22 +41,27 @@ const styles = StyleSheet.create({
   input: {
     padding: 15,
     fontSize: 16,
-    backgroundColor: "white",
-    width: "90%",
-    alignSelf: "center",
+    backgroundColor: 'white',
+    width: '90%',
+    alignSelf: 'center',
     marginTop: 20,
     borderRadius: 10,
   },
   border: {
     borderWidth: 1,
-    borderColor: "#b9b9b9",
-    backgroundColor: "#f5f5f568",
+    borderColor: '#b9b9b9',
+    backgroundColor: '#f5f5f568',
   },
   inline: {
     flexGrow: 1,
   },
   full: {
-    width: "100%",
+    width: '100%',
+  },
+  error: {
+    backgroundColor: '#ffe7e768',
+    borderColor: colors.WARNING_RED,
+    borderWidth: 1,
   },
 });
 
