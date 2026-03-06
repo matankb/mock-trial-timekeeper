954a4ad2cfe94064a505d27d3ce1d3aa2b16bcf5
Matan Kotler-Berkowitz <205matan@gmail.com>
2026-03-06 10:03:26 -0500

    Enable global team accounts feature flags

--- src/constants/feature-flags.ts
+++ src/constants/feature-flags.ts
@@ -1,4 +1,7 @@
-// Disabled October 2025, since some more work is neede to bring
+// Disabled October 2025, since some more work is needed to bring
+
+import { Platform } from 'react-native';
+
 // team accounts to GA.
-export const TEAM_ACCOUNT_PROMO_ENABLED = false;
-export const TEAM_TRIALS_IN_APP_ENABLED = false;
+export const TEAM_ACCOUNT_PROMO_ENABLED = Platform.OS !== 'web';
+export const TEAM_TRIALS_IN_APP_ENABLED = Platform.OS !== 'web';
